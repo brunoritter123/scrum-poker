14602782512e6d605366a1711729612cd96bb9d0
brunoritter123 <brunosk8123@hotmail.com>
2021-08-19 18:39:14 -0300

    test: Sala Finalizar jogo

--- ScrumPoker_Server/ScrumPoker.Tests/Application/Services/SalaServiceTests.cs
+++ ScrumPoker_Server/ScrumPoker.Tests/Application/Services/SalaServiceTests.cs
@@ -69,6 +69,57 @@ namespace ScrumPoker.Tests.Application.Services
             Assert.Contains(id.ToString(), exception.Message);
         }
 
+        [Fact(DisplayName = "Finalizar Jogo")]
+        public async void FinalizarJogoTest()
+        {
+            // Arrange
+            string id = "SalaID";
+            var sala = SalaValida(id);
+            sala.JogoFinalizado = false;
+            sala.UltimaDataDeUtilizacao = DateTime.MinValue;
+
+            var mockRepo = new Mock<ISalaRepository>();
+            mockRepo.Setup(repo => repo.BuscarPorIdAsync(id))
+                    .ReturnsAsync(sala);
+
+            var salaInputAlteracao = new Sala();
+            mockRepo.Setup(repo => repo.AlterarAsync(It.IsAny<Sala>()))
+                    .ReturnsAsync((Sala sala) => sala)
+                    .Callback((Sala sala) => salaInputAlteracao = sala);
+
+            var subject = new SalaService(mockRepo.Object, _mapper);
+
+            // Act
+            var result = await subject.FinalizarJogo(id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(id, result.Id);
+            Assert.True(result.JogoFinalizado);
+            Assert.InRange(salaInputAlteracao.UltimaDataDeUtilizacao, DateTime.Now.AddMinutes(-5), DateTime.Now);
+            Assert.Equal(sala.Titulo, result.Titulo);
+        }
+
+        [Fact(DisplayName = "Finalizar Jogo de Sala que não existe")]
+        public async void FinalizarJogoOndeSalaNaoExisteTest()
+        {
+            // Arrange
+            string id = "SalaID";
+
+            var mockRepo = new Mock<ISalaRepository>();
+            mockRepo.Setup(repo => repo.BuscarPorIdAsync(id))
+                    .ReturnsAsync((Sala)null);
+
+            var subject = new SalaService(mockRepo.Object, _mapper);
+
+            // Act
+            Func<Task> act = () => subject.FinalizarJogo(id);
+
+            // Assert
+            var exception = await Assert.ThrowsAsync<ApplicationException>(act);
+            Assert.Contains(id.ToString(), exception.Message);
+        }
+
         private Sala SalaValida(string idSala)
         {
             var sala = new Sala()
@@ -76,6 +127,7 @@ namespace ScrumPoker.Tests.Application.Services
                 Id = idSala,
                 JogoFinalizado = false,
                 UltimaDataDeUtilizacao = DateTime.Now,
+                Titulo = "Titulo"
             };
 
             sala.Configuracao = new SalaConfiguracao()
